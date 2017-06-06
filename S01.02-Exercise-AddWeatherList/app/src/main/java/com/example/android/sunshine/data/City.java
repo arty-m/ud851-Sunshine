4e1432777565db4d8c36754dc61609191397ab58
arty-m <mihir.atmakuri@gmail.com>
2017-06-05 21:27:52 -0500

    Added City list
    - added a city list and a class in the data
    - added the data to the text view

---
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/data/City.java
@@ -0,0 +1,59 @@
+package com.example.android.sunshine.data;
+
+/**
+ * Created by Mihir on 6/4/2017.
+ */
+
+public final class City {
+
+    /**
+     * This method returns a list of Cities.
+     *
+     * @return A list of popular toys
+     */
+    public static String[] getCityNames() {
+        return new String[] {
+                "Vijayawada",
+                "Eluru",
+                "Chirala",
+                "Minneapolis",
+                "Stanton",
+                "Mounds View",
+                "Guntur",
+                "Microscope Set",
+                "Beach Ball",
+                "BB Gun",
+                "Green Soldiers",
+                "Bubbles",
+                "Spring Toy",
+                "Fortune Telling Ball",
+                "Plastic Connecting Blocks",
+                "Water Balloon",
+                "Paint-by-Numbers Kit",
+                "Tuber Head",
+                "Cool Ball with Holes in It",
+                "Toy Truck",
+                "Flying Disc",
+                "Two-Handed Pogo Stick",
+                "Toy Hoop",
+                "Dysmorphia Doll",
+                "Toy Train",
+                "Fake Vomit",
+                "Toy Telephone",
+                "Barrel of Primates",
+                "Remote Control Car",
+                "Square Puzzle Cube",
+                "Football",
+                "Intergalactic Electronic Phasers",
+                "Baby Horse Dolls",
+                "Machines that turn into other Machines",
+                "Teddy Bears",
+                "Shaved Ice Maker",
+                "Glow Stick",
+                "Squirt Guns",
+                "Miniature Replica Animals Stuffed with Beads that you swore to your parents would be worth lots of money one day",
+                "Creepy Gremlin Doll",
+                "Neodymium-Magnet Toy"
+        };
+    }
+}
